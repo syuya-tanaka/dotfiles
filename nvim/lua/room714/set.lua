1e676f5e1480be606c50d9965e518f8a7947ad8d
syuya-tanaka <84017791+syuya-tanaka@users.noreply.github.com>
2024-04-18 19:51:12 +0900

    add: add netrw settings.

--- nvim/lua/room714/set.lua
+++ nvim/lua/room714/set.lua
@@ -3,7 +3,7 @@ vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
-vim.opt.shiftwidth= 4
+vim.opt.shiftwidth = 4
 
 -- comment out for golang.
 -- vim.opt.expandtab= true
@@ -27,5 +27,9 @@ vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
 
 vim.g.mapleader = " "
+
+-- netrw config
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
 
 vim.cmd("language en_US")
